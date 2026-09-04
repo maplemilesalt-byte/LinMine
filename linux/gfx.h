0901c1a25837f5e3d65c50c202872c888abd8248
Maple_Miles <maplemilesalt@gmail.com>
2026-09-04 13:22:58 -0300

    Expose platform-neutral frame delay

--- linux/gfx.h
+++ linux/gfx.h
@@ -5,18 +5,8 @@
 extern "C" {
 #endif
 
-/* Small SDL2-backed graphics layer used by the NT Minesweeper port. */
-typedef struct LMPoint {
-    int x;
-    int y;
-} LMPoint;
-
-typedef struct LMRect {
-    int x;
-    int y;
-    int width;
-    int height;
-} LMRect;
+typedef struct LMPoint { int x; int y; } LMPoint;
+typedef struct LMRect { int x; int y; int width; int height; } LMRect;
 
 typedef enum LMInputButton {
     LM_BUTTON_LEFT = 1,
@@ -32,25 +22,18 @@ typedef enum LMSpriteSheet {
 
 int lm_graphics_init(int width, int height, const char *title);
 void lm_graphics_shutdown(void);
-
-/* dir must contain the color and monochrome bitmap sets from winmine/bmp. */
 int lm_graphics_load_assets(const char *dir, int color);
 void lm_graphics_free_assets(void);
-
 void lm_graphics_begin(void);
 void lm_graphics_end(void);
 void lm_graphics_present(void);
-
+void lm_graphics_delay(unsigned int milliseconds);
 void lm_graphics_clear(unsigned int pixel);
 void lm_graphics_fill_rect(LMRect rect, unsigned int pixel);
 void lm_graphics_draw_rect(LMRect rect, unsigned int pixel);
 void lm_graphics_set_pixel(int x, int y, unsigned int pixel);
-
-/* Sprite sheets are arranged horizontally, matching the original DIBs. */
 void lm_graphics_draw_sprite(LMSpriteSheet sheet, int index, int x, int y,
                              int width, int height);
-
-/* Returns non-zero while an event was available. */
 int lm_graphics_poll_event(LMInputButton *button, LMPoint *position, int *pressed);
 int lm_graphics_should_close(void);
 
