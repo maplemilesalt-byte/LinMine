cb4f79cb66ba8b4a4d4b15a3432a28cf48fd3c52
Maple_Miles <maplemilesalt@gmail.com>
2026-09-04 14:04:58 -0300

    Add explicit graphics close operation

--- linux/gfx.h
+++ linux/gfx.h
@@ -22,6 +22,7 @@ typedef enum LMSpriteSheet {
 
 int lm_graphics_init(int width, int height, const char *title);
 void lm_graphics_shutdown(void);
+void lm_graphics_request_close(void);
 int lm_graphics_load_assets(const char *dir, int color);
 void lm_graphics_free_assets(void);
 void lm_graphics_begin(void);
@@ -36,7 +37,6 @@ void lm_graphics_draw_sprite(LMSpriteSheet sheet, int index, int x, int y,
                              int width, int height);
 void lm_graphics_draw_text(const char *text, int x, int y, unsigned int pixel);
 int lm_graphics_poll_event(LMInputButton *button, LMPoint *position, int *pressed);
-int lm_graphics_poll_key(int *key);
 int lm_graphics_should_close(void);
 
 #ifdef __cplusplus
